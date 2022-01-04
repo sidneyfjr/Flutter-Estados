f1b9d6ce2696132bdf801db281ac241ac79494a8
sidney.silva <sidney.silva@sepog.fortaleza.ce.gov.br>
2022-01-03 21:58:34 -0300

    Diseño Listo

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              child: Text('Establecer Usuario',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+              }
+              ),
+            MaterialButton(
+              child: Text('Cambiar Edad',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+              }
+              ),
+            MaterialButton(
+              child: Text('Añadir Profesion',style: TextStyle(color: Colors.white),),
+              color: Colors.blue,
+              onPressed: (){
+              }
+              ),
+          ],
+        ),
+      ),     
+    );
+  }
+}
